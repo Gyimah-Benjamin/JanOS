10d56321e4359ae1520d71b5467955cd8b73dad8
Gyimah-Benjamin <gyimah8benjamin@gmail.com>
2026-05-28 04:19:19 +0000

    Add files via upload

--- keyboard.c
+++ keyboard.c
@@ -1,6 +1,46 @@
 #include "keyboard.h"
 
+char last_key = 0;
 int shift = 0;
+
+char low[] = {
+        0, 0, '1','2','3','4','5','6','7','8','9','0','-','=','\b',
+        0,
+        'q','w','e','r','t','y','u','i','o','p','[',']',
+        '\n', 0,
+        'a','s','d','f','g','h','j','k','l',';','\'','`',
+        0, '\\',
+        'z','x','c','v','b','n','m',',','.','/',
+        0, 0, 0,
+        ' '
+    };
+
+
+char up[] = {
+          0,
+          27,
+         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+         '_', '+',
+         '\b','\t',
+
+         'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+         '{', '}','\n',0,
+
+         'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+         ':', '"', '~',0,'|',
+
+        'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+        '<', '>', '?',
+
+        0,
+
+        '*',
+
+        0,
+
+        ' ',
+
+             };
 
 void port_send_2_bytes(unsigned short port, unsigned short value){
 __asm__("out %%ax, %%dx" :: "a"(value), "d"(port));
@@ -22,84 +62,36 @@ void port_send(unsigned short port, unsigned short value) {
     __asm__("out %%al, %%dx" :: "a"(value), "d"(port));
 }
 
-char get_key() {
+void get_key() {
     unsigned char scancode;
-    while (1) {
-        unsigned char status = port_read(0x64);
-        if (status & 0x01) break;
-    }
 
     scancode = port_read(0x60);
     if(scancode == 0x2A || scancode == 0x36){
 shift = 1;
-return 0;
+return;
 }
 
 if(scancode == 0xAA || scancode == 0xB6){
 shift = 0;
-return 0;
+return;
 }
 
 if(scancode == 0xE0){
 while(!(port_read(0x64) & 0x01));
 unsigned char scancode2 = port_read(0x60);
 if(scancode2 == 0x48){
-return UP;
+last_key = UP;
+return;
 }
 else if(scancode2 == 0x50){
-return DOWN;
+last_key = DOWN;
+return;
 }
 }
-    if (scancode & 0x80) return 0;
+    if (scancode & 0x80) return;
 
-    char low[] = {
-        0, 0, '1','2','3','4','5','6','7','8','9','0','-','=','\b',
-        0,
-        'q','w','e','r','t','y','u','i','o','p','[',']',
-        '\n', 0,
-        'a','s','d','f','g','h','j','k','l',';','\'','`',
-        0, '\\',
-        'z','x','c','v','b','n','m',',','.','/',
-        0, 0, 0,
-        ' '
-    };
-
-      char up[] = {
-          0,
-          27,
-         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
-         '_', '+',
-         '\b',
-
-         '\t',
-
-         'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
-         '{', '}',
-         '\n',
-
-         0,
-
-         'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-         ':', '"', '~',
-
-         0,
-
-        '|',
-
-        'Z', 'X', 'C', 'V', 'B', 'N', 'M',
-        '<', '>', '?',
-
-        0,
-
-        '*',
-
-        0,
-
-        ' ',
-
-             };
     if (scancode < sizeof(low)){
-        return shift?up[scancode]:low[scancode];
+        last_key = shift?up[scancode]:low[scancode];
 }
-    return 0;
+    return;
 }
